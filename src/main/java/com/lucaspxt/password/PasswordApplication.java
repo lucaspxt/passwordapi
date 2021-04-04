d902a13573e85871249bfbc1807e709a7bbc8b9a
Lucas Peixoto <eng_lucas@outlook.com>
2021-04-04 16:03:29 -0300

    Alterando pacote da classe Password

--- src/main/java/com/lucaspxt/password/PasswordApplication.java
+++ src/main/java/com/lucaspxt/password/PasswordApplication.java
@@ -9,5 +9,9 @@ public class PasswordApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PasswordApplication.class, args);
 	}
-
+	
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
 }
