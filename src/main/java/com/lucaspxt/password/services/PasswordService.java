4e108398e124d7366dead74dbd171a8f8196f590
Lucas Peixoto dos Santos <42516060+lucaspxt@users.noreply.github.com>
2021-04-05 11:22:23 -0300

    Update PasswordService.java
    Removendo o caractere "-" por erros causados no regex

--- src/main/java/com/lucaspxt/password/services/PasswordService.java
+++ src/main/java/com/lucaspxt/password/services/PasswordService.java
@@ -25,13 +25,14 @@ public class PasswordService {
 	}
 	
 	public List<RegexPattern> getRegexList() {
+		
 		List<RegexPattern> list = new ArrayList<>();
-		list.add(new RegexPattern("^.*(?=.{9,}).*$", "A senha deve possuir no mínimo 9 digitos."));
-		list.add(new RegexPattern("^.*(?=.*[0-9]).*$", "A senha deve possuir ao menos um número."));
-		list.add(new RegexPattern("^.*(?=.*[a-z]).*$", "A senha deve possuir ao menos uma letra minúscula."));
-		list.add(new RegexPattern("^.*(?=.*[A-Z]).*$", "A senha deve possuir ao menos uma letra maiúscula."));
-		list.add(new RegexPattern("^(?:([\\w])(?!.*\\1))*$", "A senha não deve possuir caracteres repetidos."));
-		list.add(new RegexPattern("^.*(?=.*[!@#$%-^&+*()]).*$", "A senha deve possuir ao menos um caractere especial."));
+		list.add(new RegexPattern("^.*(?=.{9,}).*$", "A senha deve possuir no mínimo 9 digitos"));
+		list.add(new RegexPattern("^.*(?=.*[0-9]).*$", "A senha deve possuir ao menos um número"));
+		list.add(new RegexPattern("^.*(?=.*[a-z]).*$", "A senha deve possuir ao menos uma letra minúscula"));
+		list.add(new RegexPattern("^.*(?=.*[A-Z]).*$", "A senha deve possuir ao menos uma letra maiúscula"));
+		list.add(new RegexPattern("^(?:([\\w!@#$%^&+*()])(?!.*\\1))*$", "A senha não deve possuir caracteres repetidos"));
+		list.add(new RegexPattern("^.*(?=.*[!@#$%^&+*()]).*$", "A senha deve possuir ao menos um dos caracteres especiais .*[!@#$%^&+*()]"));
 		
 		return list;
 	}
