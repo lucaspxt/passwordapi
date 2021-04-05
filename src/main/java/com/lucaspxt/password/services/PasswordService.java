d3f67fe560c3cc1aac4e090e4d7fcf6ef1cf5fc1
Lucas Peixoto <eng_lucas@outlook.com>
2021-04-05 09:49:21 -0300

    Corrige regex de caractere duplicado

--- src/main/java/com/lucaspxt/password/services/PasswordService.java
+++ src/main/java/com/lucaspxt/password/services/PasswordService.java
@@ -31,7 +31,7 @@ public class PasswordService {
 		list.add(new RegexPattern("^.*(?=.*[0-9]).*$", "A senha deve possuir ao menos um número."));
 		list.add(new RegexPattern("^.*(?=.*[a-z]).*$", "A senha deve possuir ao menos uma letra minúscula."));
 		list.add(new RegexPattern("^.*(?=.*[A-Z]).*$", "A senha deve possuir ao menos uma letra maiúscula."));
-		list.add(new RegexPattern("^(?:([0-9a-zA-Z"+specialCharacters+"])(?!.*\\1))*$", "A senha não deve possuir caracteres repetidos."));
+		list.add(new RegexPattern("^(?:([0-9a-zA-Z"+specialCharacters+"])(?!.*\1))*$", "A senha não deve possuir caracteres repetidos."));
 		list.add(new RegexPattern("^.*(?=.*["+specialCharacters+"]).*$", "A senha deve possuir ao menos um caractere especial."));
 		
 		return list;
